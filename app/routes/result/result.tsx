186cd3d529ac960a3b0fbb762f392da2ad2d4f1a
yihyun-kim1 <hyunnyfarm@naver.com>
2025-05-10 21:39:27 +0900

    feat: embla를 활용한 carousel 컴포넌트 정의 및 임시 이미지 추가

--- app/routes/result/result.tsx
+++ app/routes/result/result.tsx
@@ -1,4 +1,45 @@
+import { type EmblaOptionsType } from "embla-carousel";
+import React from "react";
+import EmblaCarousel from "~/shared/components/carousel/EmblaCarousel";
+
+const OPTIONS: EmblaOptionsType = { loop: true }
+
+const createSlides = (count: number) => {
+
+  return Array.from({ length: count }, (_, index) => ({
+    id: index,
+    image: `/app/shared/images/${index + 1}.png`,
+    alt: `img-alt-${index + 1}`
+  }));
+};
+
 export default function ResultPage() {
-    return <div>result</div>
+  const [slideCount, setSlideCount] = React.useState(3); // 이번 스펙 기준 3장으로 설정
+  const slides = createSlides(slideCount);
+
+  const handleSlideCountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const count = parseInt(e.target.value);
+    if (!isNaN(count) && count > 0) {
+      setSlideCount(count);
+    }
+  };
+
+    return (
+      <div>
+        <input 
+            id="slide-count"
+            type="number" 
+            min="1"
+            value={slideCount} 
+            onChange={handleSlideCountChange}
+          />
+        <div className="carousel-container">
+          <EmblaCarousel 
+            slides={slides} 
+            options={OPTIONS}
+            showArrows={true}
+          />
+        </div>
+    </div>
+    )
   }
-  
