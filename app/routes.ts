ad6631ccba88cf2be26672f3cba3e2d0787c03ac
GyuriKim <artgr2062@konkuk.ac.kr>
2025-05-10 20:22:20 +0900

    feat: image-studio route 설정

--- app/routes.ts
+++ app/routes.ts
@@ -9,7 +9,7 @@ export default [
   layout("./shared/components/root-layout.tsx", [
     index("./routes/home.tsx"),
     route("/flavor-select", "./routes/flavor-select/flavor-select.tsx"),
-    route("/settings", "./routes/settings/image.tsx"),
+    route("/image-studio", "./routes/image-studio/image-studio.tsx"),
     route("/result", "./routes/result/result.tsx"),
   ]),
 ] satisfies RouteConfig;
