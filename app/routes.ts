2861346b4057fd407e8e8264a25bd5492d3e21f8
GyuriKim <artgr2062@konkuk.ac.kr>
2025-05-10 20:04:21 +0900

    feat: root layout 구현 및 적용

--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,15 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
-
+  layout("./shared/components/root-layout.tsx", [
+    index("./routes/home.tsx"),
     route("/flavor-select", "./routes/flavor-select/flavor-select.tsx"),
     route("/settings", "./routes/settings/image.tsx"),
     route("/result", "./routes/result/result.tsx"),
-
-
-
+  ]),
 ] satisfies RouteConfig;
