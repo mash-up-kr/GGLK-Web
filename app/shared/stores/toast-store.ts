1e54749b78ca528345efc36391a9e24dfb864c87
GyuriKim <artgr2062@konkuk.ac.kr>
2025-06-22 00:21:04 +0900

    feat(toast-store.ts): toast 상태 관리를 위한 전역 상태 구현

---
+++ app/shared/stores/toast-store.ts
@@ -0,0 +1,84 @@
+import type { Toast, ToastType } from "../types/toast";
+
+type ToastAction =
+  | {
+      type: "set";
+      payload: Toast;
+    }
+  | {
+      type: "remove";
+    };
+
+interface ToastOptions {
+  message: string;
+  type?: ToastType;
+}
+
+const reducer = (state: Toast | null, action: ToastAction) => {
+  switch (action.type) {
+    case "set":
+      return { ...action.payload };
+    case "remove":
+      return null;
+  }
+};
+
+let toastStore: Toast | null = null;
+const listeners = new Set<() => void>();
+
+const dispatch = (action: ToastAction) => {
+  toastStore = reducer(toastStore, action);
+  for (const listener of listeners) {
+    listener();
+  }
+};
+
+const subscribe = (listener: () => void) => {
+  listeners.add(listener);
+  return () => {
+    listeners.delete(listener);
+  };
+};
+
+const getToast = () => toastStore;
+
+const genIdByTime = () => new Date().getTime().toString();
+
+const createToast = ({ message, type = "plain" }: ToastOptions) => {
+  return {
+    id: genIdByTime(),
+    message,
+    type,
+  };
+};
+
+const toast = (message: string) => {
+  const newToast = createToast({
+    message,
+  });
+  dispatch({ type: "set", payload: newToast });
+};
+
+toast.success = (message: string) => {
+  const newToast = createToast({
+    message,
+    type: "success",
+  });
+  dispatch({ type: "set", payload: newToast });
+};
+
+toast.error = (message: string) => {
+  const newToast = createToast({
+    message,
+    type: "error",
+  });
+  dispatch({ type: "set", payload: newToast });
+};
+
+const toastHandler = {
+  getToast,
+  subscribe,
+  dispatch,
+};
+
+export { toast, toastHandler };
