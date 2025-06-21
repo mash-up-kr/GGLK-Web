6df4f9ee20181dd7fd03541a1f099df6d9138d7d
GyuriKim <artgr2062@konkuk.ac.kr>
2025-06-21 20:18:25 +0900

    feat: toast 관련 타입 정의 및 toast container 역할의 toaster 컴포넌트 구현

---
+++ app/shared/types/toast.ts
@@ -0,0 +1,9 @@
+export type ToastType = "success" | "error" | "plain";
+
+export type ToastPosition =
+  | "top-left"
+  | "top-center"
+  | "top-right"
+  | "bottom-left"
+  | "bottom-center"
+  | "bottom-right";
