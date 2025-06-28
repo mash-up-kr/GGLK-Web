299c6fa28baa81861199b284d300fbf4db5fd09d
GyuriKim <artgr2062@konkuk.ac.kr>
2025-06-28 15:08:03 +0900

    fix: 토스트를 빠르게 생성 시, 자동 제거가 안되는 버그 수정

--- app/shared/components/toast/toast-item.tsx
+++ app/shared/components/toast/toast-item.tsx
@@ -10,6 +10,7 @@ interface ToastItemProps {
 export default function ToastItem({ toast }: ToastItemProps) {
   const { id, message, type, offset } = toast;
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies: id가 바뀔 때마다 실행되어야하는게 맞으므로 비활성화
   useEffect(() => {
     const timerId = setTimeout(() => {
       toastHandler.dispatch({ type: "remove" });
@@ -18,15 +19,15 @@ export default function ToastItem({ toast }: ToastItemProps) {
     return () => {
       clearTimeout(timerId);
     };
-  }, []);
+  }, [id]);
 
   return (
     <motion.div
-      key={id}
+      key={id} // id가 바뀔 때마다 새로운 컴포넌트
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.3, ease: "easeInOut" }}
+      transition={{ duration: 0.2, ease: "easeInOut" }}
       className={
         "relative w-fit max-w-full rounded-3xl border bg-black px-5 py-2.5 text-center text-sm text-white"
       }
