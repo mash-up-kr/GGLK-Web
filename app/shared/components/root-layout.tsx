8f98baed5cdf6b6555fbebea9dd13741aaeffba4
GyuriKim <artgr2062@konkuk.ac.kr>
2025-06-28 23:29:53 +0900

    feat(root-layout): root Layout에 relative 클래스 추가

--- app/shared/components/root-layout.tsx
+++ app/shared/components/root-layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export default function RootLayout() {
   return (
     <div className="flex h-screen flex-col items-center">
-      <div className="h-full w-full max-w-3xl">
+      <div className="relative h-full w-full max-w-3xl">
         <Outlet />
       </div>
     </div>
