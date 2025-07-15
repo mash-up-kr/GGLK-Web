cd639e77a488908ec78b10e91527b1777b2f9e8a
GyuriKim <artgr2062@konkuk.ac.kr>
2025-07-16 02:34:16 +0900

    feat: 게스트 토큰 쿠키 설정 axios interceptor 설정

--- app/api/mutator/custom-instance.ts
+++ app/api/mutator/custom-instance.ts
@@ -1,6 +1,7 @@
 import Axios, { type AxiosError, type AxiosRequestConfig } from "axios";
 import { Cookies } from "react-cookie";
 import { AUTH_KEY } from "~/shared/constants";
+import { authControllerGuestToken } from "../endpoints/api";
 
 const cookies = new Cookies();
 
@@ -22,6 +23,34 @@ AXIOS_INSTANCE.interceptors.request.use((config) => {
   return config;
 });
 
+AXIOS_INSTANCE.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const token = cookies.get(AUTH_KEY);
+    const { config, response } = error;
+
+    if (
+      response.status === 401 &&
+      config.url !== "/auth/guest" &&
+      !config.sent &&
+      !token
+    ) {
+      config.sent = true;
+
+      const {
+        data: { token: guestToken },
+      } = await authControllerGuestToken();
+
+      if (guestToken) {
+        config.headers.Authorization = `Bearer ${token}`;
+        cookies.set(AUTH_KEY, guestToken);
+      }
+      return AXIOS_INSTANCE(config);
+    }
+
+    return Promise.reject(error);
+  },
+);
 export const customInstance = <T>(
   config: AxiosRequestConfig,
 ): Promise<ResponseType<T>> => {
--- app/routes/analyze/image-studio.tsx
+++ app/routes/analyze/image-studio.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { type Path, useFormContext } from "react-hook-form";
 import { usePictureControllerUploadPicture } from "~/api/endpoints/api";
-import { useAuthentication } from "~/shared/hooks/use-authentication";
 import { toast } from "~/shared/stores/toast-store";
 import { convertHeicToJpeg, isHeicFile } from "~/shared/utils/image-utils";
 import type { AnalyzeFormData } from "./analyze";
@@ -21,8 +20,8 @@ export default function ImageStudioPage({
   const closeModalRef = useRef<HTMLButtonElement>(null);
   const [tempImageUrl, setTempImageUrl] = useState<string | null>(null);
   const [isConverting, setIsConverting] = useState(false);
-  // FIXME: 게스트 토큰 쿠키 생성 미들웨어로 변경
-  const { isAuthenticated } = useAuthentication();
+  // // FIXME: 게스트 토큰 쿠키 생성 미들웨어로 변경
+  // const { isAuthenticated } = useAuthentication();
 
   const {
     mutate: uploadPicture,
