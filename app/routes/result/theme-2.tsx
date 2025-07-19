a9373382543818a81c17349e62e6bbe7f73c0b23
yihyun-kim1 <hyunnyfarm@naver.com>
2025-07-19 10:50:22 +0900

    fix: theme1 design QA

--- app/routes/result/theme-2.tsx
+++ app/routes/result/theme-2.tsx
@@ -79,32 +79,32 @@ export default function Theme2({
   return (
     <div className="relative h-full w-full overflow-hidden">
       <div className="absolute inset-0 z-10">
-        <div className="absolute top-12 left-20">
+        <div className="absolute top-12 left-15">
           <img
             src="/png/iconTodaysLook.png"
             className="h-[82px] w-[238px] rotate-3"
             alt="iconTodaysLook"
           />
         </div>
-        <div className="absolute top-12 right-20">
+        <div className="absolute top-12 right-25">
           <img
             src="/png/iconStarPearl.png"
             className="h-[48px] w-[48px]"
             alt="iconStarPearl"
           />
         </div>
-        <div className="absolute top-12 right-15">
+        <div className="absolute top-12 right-20">
           <img
             src="/png/iconStarPearl.png"
             className="h-[30px] w-[30px]"
             alt="iconStarPearl"
           />
         </div>
-        <div className="absolute top-10 right-0">
+        <div className="absolute top-10 right-5">
           <img
-            src="/png/iconTagNormal.png"
+            src="/png/iconTagSpicy.png"
             className="h-[21px] w-[63px]"
-            alt="iconTagNormal"
+            alt="iconTagSpicy"
           />
         </div>
       </div>
